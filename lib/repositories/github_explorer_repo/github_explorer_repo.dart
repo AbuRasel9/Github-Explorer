f5a44d8618a39f44fa619a1654403acdbe2992cf
AbuRasel51 <rasel@primetechbd.com>
2025-11-05 18:41:20 +0600

    repo model add github_explorer_repo complete

--- lib/repositories/github_explorer_repo/github_explorer_repo.dart
+++ lib/repositories/github_explorer_repo/github_explorer_repo.dart
@@ -1,3 +1,8 @@
+import 'package:github_explorer/models/repo_model.dart';
+import 'package:github_explorer/models/user_model.dart';
+
 abstract class GithubExplorerRepo{
-  Future<>
+  Future<UserModel>fetchUser(String userName);
+  Future<List<RepoModel>>fetchRepos(String userName);
+
 }
